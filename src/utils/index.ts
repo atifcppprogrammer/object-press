284607e1f226f6b4b2962a5307661ca30e70c133
vincent <vmcodes05@gmail.com>
2022-03-17 20:43:13 -0400

    add regex utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const slugify = (text: string) => {
+export const slugify = (text: string): string => {
   return text
     .toString()
     .toLowerCase()
@@ -7,4 +7,18 @@ export const slugify = (text: string) => {
     .replace(/--+/g, '-') // Replace multiple - with single -
     .replace(/^-+/, '') // Trim - from start of text
     .replace(/-+$/, ''); // Trim - from end of text
+};
+
+export const isUrl = (url: string): boolean => {
+  const regex =
+    // eslint-disable-next-line
+    /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+
+  return regex.test(url);
+};
+
+export const isLength = (value: string, length: number): boolean => {
+  const validLength = value.length <= length;
+
+  return validLength;
 };
