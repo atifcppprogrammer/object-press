53cd0b3c405a2e80da2c42bad3ae24af1ad6dcf2
vincent <vmcodes05@gmail.com>
2022-03-25 21:38:27 -0400

    aws authstate

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import {
   AmplifyAuthenticator,
@@ -6,7 +6,7 @@ import {
   AmplifySignIn,
 } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify';
-import { onAuthUIStateChange } from '@aws-amplify/ui-components';
+import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components';
 import { useMutation } from '@apollo/client';
 import { LOGIN_MUTATION } from 'graphql/mutations';
 import { Redirect } from 'react-router-dom';
@@ -14,10 +14,12 @@ import { AuthContext } from 'context/auth';
 import Topbar from './Topbar/Topbar';
 import Footer from './Footer/Footer';
 import './styles/index.css';
+import { InLineLoader } from 'components/InlineLoader/InlineLoader';
 
 export default function Login() {
   const [login] = useMutation(LOGIN_MUTATION);
   const { isAuthenticated } = useContext(AuthContext);
+  const [authState, setAuthState] = useState(AuthState.SignIn);
 
   async function emailSession() {
     try {
@@ -65,6 +67,8 @@ export default function Login() {
 
   useEffect(() => {
     return onAuthUIStateChange(async (nextAuthState, authData: any) => {
+      setAuthState(nextAuthState);
+
       if (authData?.id) {
         await federatedSession(authData);
       } else if (authData?.username) {
@@ -87,47 +91,51 @@ export default function Login() {
       <Topbar />
       <main style={{ background: '#f7faff' }}>
         <section className="relative w-full h-full py-40 min-h-screen">
-          <AmplifyAuthenticator
-            usernameAlias="email"
-            federated={{
-              googleClientId:
-                '1035201445904-5p974ebq7aasf411ammemut36m7vf5bb.apps.googleusercontent.com',
-            }}
-          >
-            <AmplifySignUp
-              slot="sign-up"
+          {authState !== AuthState.SignIn ? (
+            <InLineLoader />
+          ) : (
+            <AmplifyAuthenticator
               usernameAlias="email"
-              formFields={[
-                {
-                  type: 'email',
-                  label: 'Email Address *',
-                  placeholder: 'Enter your email address',
-                  inputProps: {
-                    required: true,
-                    autocomplete: 'email',
-                  },
-                },
-                {
-                  type: 'password',
-                  label: 'Password *',
-                  placeholder: 'Enter your password',
-                  inputProps: {
-                    required: true,
-                    autocomplete: 'new-password',
-                  },
-                },
-              ]}
-            />
-            <AmplifySignIn
-              slot="sign-in"
-              usernameAlias="email"
-              federated-buttons={true}
               federated={{
                 googleClientId:
                   '1035201445904-5p974ebq7aasf411ammemut36m7vf5bb.apps.googleusercontent.com',
               }}
-            />
-          </AmplifyAuthenticator>
+            >
+              <AmplifySignUp
+                slot="sign-up"
+                usernameAlias="email"
+                formFields={[
+                  {
+                    type: 'email',
+                    label: 'Email Address *',
+                    placeholder: 'Enter your email address',
+                    inputProps: {
+                      required: true,
+                      autocomplete: 'email',
+                    },
+                  },
+                  {
+                    type: 'password',
+                    label: 'Password *',
+                    placeholder: 'Enter your password',
+                    inputProps: {
+                      required: true,
+                      autocomplete: 'new-password',
+                    },
+                  },
+                ]}
+              />
+              <AmplifySignIn
+                slot="sign-in"
+                usernameAlias="email"
+                federated-buttons={true}
+                federated={{
+                  googleClientId:
+                    '1035201445904-5p974ebq7aasf411ammemut36m7vf5bb.apps.googleusercontent.com',
+                }}
+              />
+            </AmplifyAuthenticator>
+          )}
         </section>
       </main>
       <Footer />
